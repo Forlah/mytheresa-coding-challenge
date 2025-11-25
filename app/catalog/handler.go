f467cc01cdebd463e3459d4e2cdda56fe68aed01
fola oyewole <fola057@gmail.com>
2025-11-25 22:52:03 +0100

    implemented offset based pagination

--- app/catalog/handler.go
+++ app/catalog/handler.go
@@ -2,6 +2,7 @@ package catalog
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/mytheresa/go-hiring-challenge/app/api"
 	"github.com/mytheresa/go-hiring-challenge/app/database"
@@ -9,11 +10,13 @@ import (
 
 type Response struct {
 	Products []Product `json:"products"`
+	Total    *int64    `json:"total,omitempty"`
 }
 
 type Product struct {
-	Code  string  `json:"code"`
-	Price float64 `json:"price"`
+	Code     string  `json:"code"`
+	Price    float64 `json:"price"`
+	Category string  `json:"category"`
 }
 
 type CatalogHandler struct {
@@ -27,7 +30,28 @@ func NewCatalogHandler(r database.ProductsStore) *CatalogHandler {
 }
 
 func (h *CatalogHandler) HandleGet(w http.ResponseWriter, r *http.Request) {
-	res, err := h.productsRepo.GetAllProducts()
+	filter := database.ProductFilter{}
+
+	query := r.URL.Query()
+
+	strOffset := query.Get("offset")
+	strLimit := query.Get("limit")
+
+	if strOffset != "" {
+		if parsedOffset, err := strconv.ParseInt(strOffset, 10, 0); err == nil {
+			offset := int(parsedOffset)
+			filter.Offset = &offset
+		}
+	}
+
+	if strLimit != "" {
+		if parsedLimit, err := strconv.ParseInt(strLimit, 10, 0); err == nil {
+			limit := int(parsedLimit)
+			filter.Limit = &limit
+		}
+	}
+
+	res, count, err := h.productsRepo.GetAllProducts(filter)
 	if err != nil {
 		api.ErrorResponse(w, http.StatusInternalServerError, "Failed to retrieve products")
 		return
@@ -37,14 +61,16 @@ func (h *CatalogHandler) HandleGet(w http.ResponseWriter, r *http.Request) {
 	products := make([]Product, len(res))
 	for i, p := range res {
 		products[i] = Product{
-			Code:  p.Code,
-			Price: p.Price.InexactFloat64(),
+			Code:     p.Code,
+			Price:    p.Price.InexactFloat64(),
+			Category: p.ProductCategory.Name,
 		}
 	}
 
 	// Return the products as a JSON response
 	response := Response{
 		Products: products,
+		Total:    count,
 	}
 
 	api.OKResponse(w, response)
--- app/database/products_repository.go
+++ app/database/products_repository.go
@@ -9,8 +9,13 @@ type productsRepository struct {
 	db *gorm.DB
 }
 
+type ProductFilter struct {
+	Offset *int
+	Limit  *int
+}
+
 type ProductsStore interface {
-	GetAllProducts() ([]models.Product, error)
+	GetAllProducts(filters ProductFilter) ([]models.Product, *int64, error)
 }
 
 func NewProductsRepository(db *gorm.DB) *productsRepository {
@@ -19,10 +24,48 @@ func NewProductsRepository(db *gorm.DB) *productsRepository {
 	}
 }
 
-func (r *productsRepository) GetAllProducts() ([]models.Product, error) {
-	var products []models.Product
-	if err := r.db.Preload("Variants").Find(&products).Error; err != nil {
-		return nil, err
+func (r *productsRepository) GetAllProducts(filters ProductFilter) ([]models.Product, *int64, error) {
+	const (
+		maxLimit     = 100
+		minLimit     = 1
+		defaultLimit = 10
+	)
+
+	offset := 0
+	limit := defaultLimit
+
+	if filters.Offset != nil {
+		offset = *filters.Offset
 	}
-	return products, nil
+
+	if filters.Limit != nil {
+		limit = *filters.Limit
+	}
+
+	if limit > maxLimit {
+		limit = maxLimit
+	} else if limit < 0 {
+		limit = minLimit
+	}
+
+	var products []models.Product
+	var productCount int64
+
+	query := r.db.
+		Preload("Variants").
+		Preload("ProductCategory")
+
+	if err := query.Model(&models.Product{}).Count(&productCount).Error; err != nil {
+		return nil, nil, err
+	}
+
+	if err := query.
+		Order("id ASC").
+		Offset(offset).
+		Limit(limit).
+		Find(&products).Error; err != nil {
+		return nil, nil, err
+	}
+
+	return products, &productCount, nil
 }
